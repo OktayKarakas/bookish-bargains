2ec5dcb46c59e86e3cdb477624dbe536e391db20
Oktay Karakaş <109868198+OktayKarakas@users.noreply.github.com>
2023-05-07 14:05:40 +0300

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,4 @@
 import "@/styles/globals.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import type { AppProps } from "next/app";
 import Router from "next/router";
 import ProgressBar from "@badrap/bar-of-progress";
